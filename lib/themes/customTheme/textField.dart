e7e8f1b4d5fb46e9ff97f7e3347f07ad0f863891
Naman2004singh <naman.2singh9@gmail.com>
2024-11-14 20:30:23 +0530

    added the theme style for text field

---
+++ lib/themes/customTheme/textField.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hola_app/themes/colors.dart';
+import 'package:hola_app/themes/customTheme/textTheme.dart';
+
+class textField {
+  textField._();
+
+  static InputDecorationTheme textFieldTheme = InputDecorationTheme(
+    prefixIconColor: colors.mainColor,
+    suffixIconColor: colors.whiteColor,
+    hintStyle: textTheme.apptextTheme.bodyLarge,
+    enabledBorder: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(width: 1,color: colors.whiteColor)
+    ),
+    focusedBorder: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(width: 1,color: colors.mainColor)
+    ),
+  );
+}
--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hola_app/themes/customTheme/textField.dart';
 import 'package:hola_app/themes/customTheme/textTheme.dart';
 
 class AppTheme {
   AppTheme._();
 
   static ThemeData themeData = ThemeData(
-    textTheme: textTheme.apptextTheme
+    useMaterial3: true,
+    textTheme: textTheme.apptextTheme,
+    inputDecorationTheme: textField.textFieldTheme,
+    
   );
 }

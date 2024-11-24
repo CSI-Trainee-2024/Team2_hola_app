cea19b30b26c84077d935fc7023456c4e4bc7de3
Naman2004singh <naman.2singh9@gmail.com>
2024-11-24 12:35:53 +0530

    created model for the video playing api and update the yaml file

---
+++ lib/themes/models/videoApi.dart
@@ -0,0 +1,56 @@
+class videpapi {
+  String? id;
+  String? title;
+  String? thumbnailUrl;
+  String? duration;
+  String? uploadTime;
+  String? views;
+  String? author;
+  String? videoUrl;
+  String? description;
+  String? subscriber;
+  bool? isLive;
+
+  videpapi(
+      {this.id,
+      this.title,
+      this.thumbnailUrl,
+      this.duration,
+      this.uploadTime,
+      this.views,
+      this.author,
+      this.videoUrl,
+      this.description,
+      this.subscriber,
+      this.isLive});
+
+  videpapi.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    thumbnailUrl = json['thumbnailUrl'];
+    duration = json['duration'];
+    uploadTime = json['uploadTime'];
+    views = json['views'];
+    author = json['author'];
+    videoUrl = json['videoUrl'];
+    description = json['description'];
+    subscriber = json['subscriber'];
+    isLive = json['isLive'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['thumbnailUrl'] = thumbnailUrl;
+    data['duration'] = duration;
+    data['uploadTime'] = uploadTime;
+    data['views'] = views;
+    data['author'] = author;
+    data['videoUrl'] = videoUrl;
+    data['description'] = description;
+    data['subscriber'] = subscriber;
+    data['isLive'] = isLive;
+    return data;
+  }
+}

7327e9f18480b87139476e1b36039d21595f6498
Naman2004singh <naman.2singh9@gmail.com>
2024-11-29 20:27:26 +0530

    added new trending dart file with some changes

--- lib/subScreen/explore/trendingNotLoad.dart
+++ lib/subScreen/explore/trendingNotLoad.dart
@@ -3,14 +3,57 @@ import 'package:hola_app/subScreen/api/exploreApi.dart';
 import 'package:hola_app/themes/colors.dart';
 import 'package:hola_app/themes/customTheme/textTheme.dart';
 
-class trendingPage extends StatefulWidget {
-  const trendingPage({super.key});
+class TrendingNotLoad extends StatefulWidget {
+  const TrendingNotLoad({super.key});
 
   @override
-  State<trendingPage> createState() => _trendingPageState();
+  State<TrendingNotLoad> createState() => _TrendingNotLoadState();
 }
 
-class _trendingPageState extends State<trendingPage> with AutomaticKeepAliveClientMixin<trendingPage> {
+class _TrendingNotLoadState extends State<TrendingNotLoad>
+    with AutomaticKeepAliveClientMixin<TrendingNotLoad> {
+  List<String> items = [];
+  bool isLoading = false;
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(scrollListner);
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await Future.delayed(Duration(seconds: 2));
+
+    List<String> newItems =
+        List.generate(20, (index) => 'Item ${items.length + index + 1}');
+    setState(() {
+      items.addAll(newItems);
+      isLoading = false;
+    });
+  }
+
+  void scrollListner() {
+    if (scrollController.position.pixels ==
+        scrollController.position.maxScrollExtent) {
+      if (!isLoading) {
+        loadData();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(scrollListner);
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -38,19 +81,22 @@ class _trendingPageState extends State<trendingPage> with AutomaticKeepAliveClie
               );
             } else {
               return GridView.builder(
-                  itemCount: exploreList.length,
+                controller: scrollController,
+                  itemCount: exploreList.length + (isLoading ? 1:0),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
                       childAspectRatio: 1.5,
                       mainAxisSpacing: 10,
                       crossAxisSpacing: 10),
                   itemBuilder: (context, index) {
+                    if (index == exploreList.length && isLoading) {
+                      return const Center(child: CircularProgressIndicator(color: colors.mainColor));
+                    }
                     return ClipRRect(
                       borderRadius: BorderRadius.circular(15),
                       child: Image.network(
-                        fit: BoxFit.cover,
-                        exploreList[index].downloadUrl.toString()
-                      ),
+                          fit: BoxFit.cover,
+                          exploreList[index].downloadUrl.toString()),
                     );
                   });
             }
@@ -59,7 +105,8 @@ class _trendingPageState extends State<trendingPage> with AutomaticKeepAliveClie
       ],
     );
   }
-  
+
   @override
   bool get wantKeepAlive => true;
 }
+

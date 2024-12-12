f140259c94e7fcc733a7d539ac9660a8b9ef0b20
Naman2004singh <naman.2singh9@gmail.com>
2024-12-12 15:32:59 +0530

    added the splash screen and navigating the user to home screen if already logged in

--- lib/Screens/splashscreen.dart
+++ lib/Screens/splashscreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:hola_app/loginScreens/components/background.dart';
+import 'package:hola_app/loginScreens/loginStatus/checkLogin.dart';
+import 'package:hola_app/loginScreens/register.dart';
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
@@ -9,7 +13,26 @@ class Splashscreen extends StatefulWidget {
 
 class _SplashscreenState extends State<Splashscreen> {
   @override
+  void initState() {
+    super.initState();
+    checkLogin(context);
+    Timer(
+      const Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const registorScreen(),
+            ));
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return background(
+        provideChild: Center(
+      child: Image.asset('assets/images/hola.png'),
+    ));
   }
 }
